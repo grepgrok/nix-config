42e89be1346c128bf83dc23c281c5f5f12767d6f
grepgrok <ben.k.olson@gmail.com>
2024-09-02 17:04:38 -0700

    the symlinks went to the store, so functionally useless :(

--- modules/home-manager/hypr/default.nix
+++ modules/home-manager/hypr/default.nix
@@ -6,7 +6,10 @@
   };
 
   config = lib.mkIf config.hypr.enable {
-    home.file.".config/hypr/hyprland.conf".source = config.lib.file.mkOutOfStoreSymlink ./hyprland.conf;
-    home.file.".config/hypr/start.sh".source = config.lib.file.mkOutOfStoreSymlink ./start.sh;
+    # TODO: This links into the store, not under $HOME. :(
+    #home.file.".config/hypr/hyprland.conf".source = config.lib.file.mkOutOfStoreSymlink ./hyprland.conf;
+    #home.file.".config/hypr/start.sh".source = config.lib.file.mkOutOfStoreSymlink ./start.sh;
+    home.file.".config/hypr/hyprland.conf".source = lib.mkDefault ./hyprland.conf;
+    home.file.".config/hypr/start.sh".source = lib.mkDefault ./start.sh;
   };
 }
